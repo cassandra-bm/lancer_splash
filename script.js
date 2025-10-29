71345eb3e43f24aa9c7795d4e9775465a64ee65c
Cassandra Coal <cassie.coal@gmail.com>
2025-10-29 12:26:06 -0500

    Add and properly link 2048 cart. Update CS and site layout

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++){
-        tabcontent[i].style.display = "none";
-    }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-    tablinks = document.getElementsByClassName("tablinks");
-    for(i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+  document.getElementById(tabName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
 
-    document.getElementById(tabName).style.display = "flex";
-    evt.currentTarget.className += " active";
+async function loadSection(sectionName) {
+  const response = await fetch(`sections/${sectionName}.html`);
+  const html = await response.text();
+  document.getElementById("content").innerHTML = html;
 }
